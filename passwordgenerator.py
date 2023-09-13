d77db99072382b66b66059587ffb7e4da8b01976
SaiSrinivasa1204 <122857578+SaiSrinivasa1204@users.noreply.github.com>
2023-09-13 09:53:37 +0530

    Delete passwordgenerator.py

--- passwordgenerator.py
+++
@@ -1,5 +0,0 @@
-import random
-password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=-"
-length_password = int(input("Enter the length of the password: "))
-a = "".join(random.sample(password, length_password))
-print(f"Your password is {a}")
