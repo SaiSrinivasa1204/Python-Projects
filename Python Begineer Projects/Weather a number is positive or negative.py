c0c83a886ba637c313b1f3ddc46c34befbe58cfb
SaiSrinivasa1204 <122857578+SaiSrinivasa1204@users.noreply.github.com>
2023-09-13 10:20:55 +0530

    Add files via upload

---
+++ Python Begineer Projects/Weather a number is positive or negative.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+num = int(input("Please enter a number: "))
+if num > 0:
+    print("The entered number is positive")
+elif num < 0:
+    print("The entered number is negative")
+else:
+    print("The entered number is Zero")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
