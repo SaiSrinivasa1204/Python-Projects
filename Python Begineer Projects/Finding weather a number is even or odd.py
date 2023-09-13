f978f67ecd297e458438f28d020003818e0d58c1
SaiSrinivasa1204 <122857578+SaiSrinivasa1204@users.noreply.github.com>
2023-09-13 10:32:01 +0530

    Add files via upload

---
+++ Python Begineer Projects/Finding weather a number is even or odd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Using brute force
+num = int(input("Please enter a number: "))
+if num%2 == 0:
+    print("The entered number is Even")
+else:
+    print("The entered number is Odd")
+
+
+# In[6]:
+
+
+#Using ternary operators
+num = int(input("Please enter a number: "))
+print("Even" if num%2 == 0 else "odd")
+
+
+# In[ ]:
+
+
+
+
