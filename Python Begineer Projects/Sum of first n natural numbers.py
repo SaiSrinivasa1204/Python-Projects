5e8b4062157a7d72e741707f81783e09eca2ceb2
SaiSrinivasa1204 <122857578+SaiSrinivasa1204@users.noreply.github.com>
2023-09-13 10:44:13 +0530

    Add files via upload

---
+++ Python Begineer Projects/Sum of first n natural numbers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Using for loop
+num = int(input("Please enter the number: "))
+sum = 0
+for i in range (num+1):
+    sum+=i
+print(sum)
+
+
+# In[4]:
+
+
+#using formula
+num = int(input("Please enter a number: "))
+print(int(num*(num+1)/2))
+
+
+# In[ ]:
+
+
+
+
